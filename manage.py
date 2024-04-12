8b6b6fd3686b5d9b985e1896efdca807a964a2c7
EC2 Default User <ec2-user@ip-172-31-38-74.eu-west-2.compute.internal>
2024-04-12 13:51:34 +0000

    all

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
+"""manage.py"""
 import os
 import sys
 from django.core.management import execute_from_command_line
 
 def main():
+    """Run task"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petcare.settings')
     try:
         execute_from_command_line(sys.argv)
